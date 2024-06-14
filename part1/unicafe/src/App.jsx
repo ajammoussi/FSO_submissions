b5625f73be44682761543335503c0ae36f74134f
ajammoussi <jam.ahmed55@gmail.com>
2024-06-14 20:40:35 +0100

    exercise 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,12 +9,18 @@ const Button = (props) => (
 const StatisticLine = ({ text, value }) => {
     if (text === 'positive') {
         return (
-            <div>{text} {value} %</div>
+            <tr>
+                <td>{text}</td>
+                <td>{value} %</td>
+            </tr>
         )
     }
 
     return (
-        <div>{text} {value}</div>
+        <tr>
+            <td>{text}</td>
+            <td>{value}</td>
+        </tr>
     )
 
 }
@@ -34,14 +40,18 @@ const Statistics = ({ good, neutral, bad }) => {
     }
 
     return (
-        <div>
-            <StatisticLine text="good" value={good} />
-            <StatisticLine text="neutral" value={neutral} />
-            <StatisticLine text="bad" value={bad} />
-            <StatisticLine text="all" value={all} />
-            <StatisticLine text="average" value={average} />
-            <StatisticLine text="positive" value={positive} />
-        </div>
+        <>
+            <table>
+                <tbody>
+                <StatisticLine text="good" value={good}/>
+                <StatisticLine text="neutral" value={neutral}/>
+                <StatisticLine text="bad" value={bad}/>
+                <StatisticLine text="all" value={all}/>
+                <StatisticLine text="average" value={average}/>
+                <StatisticLine text="positive" value={positive}/>
+                </tbody>
+            </table>
+        </>
     )
 
 }
