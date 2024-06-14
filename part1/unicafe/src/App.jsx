07234087a51b26da6268e065866e806cbbc36759
ajammoussi <jam.ahmed55@gmail.com>
2024-06-14 20:22:44 +0100

    exercise 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,17 +11,39 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [all, setAll] = useState(0)
+
+    const average = (good - bad) / all
+    const positive = good / all * 100
+
+    const handleGoodClick = () => {
+        setGood(good + 1)
+        setAll(all + 1)
+    }
+
+    const handleNeutralClick = () => {
+        setNeutral(neutral + 1)
+        setAll(all + 1)
+    }
+
+    const handleBadClick = () => {
+        setBad(bad + 1)
+        setAll(all + 1)
+    }
 
     return (
         <div>
-            <h1>give feedback</h1>
-            <Button handleClick={() => setGood(good + 1)} text="good" />
-            <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-            <Button handleClick={() => setBad(bad + 1)} text="bad" />
-            <h1>statistics</h1>
+            <h2>give feedback</h2>
+            <Button handleClick={handleGoodClick} text="good" />
+            <Button handleClick={handleNeutralClick} text="neutral" />
+            <Button handleClick={handleBadClick} text="bad" />
+            <h2>statistics</h2>
             <div>good {good}</div>
             <div>neutral {neutral}</div>
             <div>bad {bad}</div>
+            <div>all {all}</div>
+            <div>average {average}</div>
+            <div>positive {positive} %</div>
         </div>
     )
 }
